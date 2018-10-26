7fcf5fb0a237ef1fdac40d086511026ff263c029
zhan0903 <z.han0903@qq.com>
2018-10-26 11:11:58 +1100

    22-10 version, debugging+he init

--- gpu_implementation/neuroevolution/models/base.py
+++ gpu_implementation/neuroevolution/models/base.py
@@ -90,6 +90,118 @@ def he_normal(shape, name=None):
     return normal(shape, s, name=name)
 
 
+def calculate_gain(nonlinearity, param=None):
+    r"""Return the recommended gain value for the given nonlinearity function.
+    The values are as follows:
+
+    ================= ====================================================
+    nonlinearity      gain
+    ================= ====================================================
+    Linear / Identity :math:`1`
+    Conv{1,2,3}D      :math:`1`
+    Sigmoid           :math:`1`
+    Tanh              :math:`\frac{5}{3}`
+    ReLU              :math:`\sqrt{2}`
+    Leaky Relu        :math:`\sqrt{\frac{2}{1 + \text{negative_slope}^2}}`
+    ================= ====================================================
+
+    Args:
+        nonlinearity: the non-linear function (`nn.functional` name)
+        param: optional parameter for the non-linear function
+
+    Examples:
+        >>> gain = nn.init.calculate_gain('leaky_relu')
+    """
+    linear_fns = ['linear', 'conv1d', 'conv2d', 'conv3d', 'conv_transpose1d', 'conv_transpose2d', 'conv_transpose3d']
+    if nonlinearity in linear_fns or nonlinearity == 'sigmoid':
+        return 1
+    elif nonlinearity == 'tanh':
+        return 5.0 / 3
+    elif nonlinearity == 'relu':
+        return math.sqrt(2.0)
+    elif nonlinearity == 'leaky_relu':
+        if param is None:
+            negative_slope = 0.01
+        elif not isinstance(param, bool) and isinstance(param, int) or isinstance(param, float):
+            # True/False are instances of int, hence check above
+            negative_slope = param
+        else:
+            raise ValueError("negative_slope {} not a valid number".format(param))
+        return math.sqrt(2.0 / (1 + negative_slope ** 2))
+    else:
+        raise ValueError("Unsupported nonlinearity {}".format(nonlinearity))
+
+
+
+def _calculate_fan_in_and_fan_out(tensor):
+    dimensions = tensor.ndimension()
+    print("dimension:",dimensions)
+    if dimensions < 2:
+        raise ValueError("Fan in and fan out can not be computed for tensor with less than 2 dimensions")
+
+    if dimensions == 2:  # Linear
+        fan_in = tensor.size(1)
+        fan_out = tensor.size(0)
+    else:
+        num_input_fmaps = tensor.size(1)
+        print("num_input_fmaps:",num_input_fmaps)
+        num_output_fmaps = tensor.size(0)
+        print("num_output_fmaps:",num_output_fmaps)
+        receptive_field_size = 1
+        if tensor.dim() > 2:
+            receptive_field_size = tensor[0][0].numel()
+        fan_in = num_input_fmaps * receptive_field_size
+        fan_out = num_output_fmaps * receptive_field_size
+
+    return fan_in, fan_out
+
+
+def _calculate_correct_fan(tensor, mode):
+    mode = mode.lower()
+    valid_modes = ['fan_in', 'fan_out']
+    if mode not in valid_modes:
+        raise ValueError("Mode {} not supported, please use one of {}".format(mode, valid_modes))
+
+    fan_in, fan_out = _calculate_fan_in_and_fan_out(tensor)
+    return fan_in if mode == 'fan_in' else fan_out
+
+
+def kaiming_normal_(tensor, a=0, mode='fan_in', nonlinearity='leaky_relu'):
+    r"""Fills the input `Tensor` with values according to the method
+    described in "Delving deep into rectifiers: Surpassing human-level
+    performance on ImageNet classification" - He, K. et al. (2015), using a
+    normal distribution. The resulting tensor will have values sampled from
+    :math:`\mathcal{N}(0, \text{std})` where
+
+    .. math::
+        \text{std} = \sqrt{\frac{2}{(1 + a^2) \times \text{fan_in}}}
+
+    Also known as He initialization.
+
+    Args:
+        tensor: an n-dimensional `torch.Tensor`
+        a: the negative slope of the rectifier used after this layer (0 for ReLU
+            by default)
+        mode: either 'fan_in' (default) or 'fan_out'. Choosing `fan_in`
+            preserves the magnitude of the variance of the weights in the
+            forward pass. Choosing `fan_out` preserves the magnitudes in the
+            backwards pass.
+        nonlinearity: the non-linear function (`nn.functional` name),
+            recommended to use only with 'relu' or 'leaky_relu' (default).
+
+    Examples:
+        >>> w = torch.empty(3, 5)
+        >>> nn.init.kaiming_normal_(w, mode='fan_out', nonlinearity='relu')
+    """
+    fan = _calculate_correct_fan(tensor, mode)
+    print()
+    gain = calculate_gain(nonlinearity, a)
+    std = gain / math.sqrt(fan)
+    print("std in kaiming", std)
+    with torch.no_grad():
+        return tensor.normal_(0, std)
+
+
 class BaseModel(object):
     def __init__(self):
         self.nonlin = tf.nn.relu
@@ -267,28 +379,26 @@ class BaseModel(object):
         logger.debug("come here in init scale_by!!!")
         shapes = []
         ran_num = np.random.randint(1, 2)
-        shape_out = [v.value for v in self.variables[-1].get_shape()][-1]
+        # shape_out = [v.value for v in self.variables[-1].get_shape()][-1]
         logger.debug("in make_weight87~~~~~~~~~~_____, shape_out:{}".format(shape_out))
-        if ran_num == 0:
-            net = Net((4, 84, 84), shape_out)
-            for p in net.parameters():
-                self.num_params += np.prod(p.data.size())
-
-                self.scale_by.append(p.data.numpy().flatten())
-
-
-
+        # if ran_num == 0:
+        #     net = Net((4, 84, 84), shape_out)
+        #     for p in net.parameters():
+        #         self.num_params += np.prod(p.data.size())
+        #
+        #         self.scale_by.append(p.data.numpy().flatten())
 
         for var in self.variables:
             shape = [v.value for v in var.get_shape()]
             shapes.append(shape)
             logger.debug("in make_weights,shape:{}".format(shape))
             self.num_params += np.prod(shape[1:])
-            if ran_num == 0:
+            if ran_num == 1:
                 # add He initialization
-                parameters = he_normal(shape[1:]).flatten()
-
-
+                w = torch.empty(shape[1:])
+                kaiming_normal_(w,mode='fan_in', nonlinearity='relu')
+                parameters = w.numpy().flatten()
+                # parameters = he_normal(shape[1:]).flatten()
                 logger.debug("in make_weights, he init shape:{0}".format(shape[1:]))
                 # logger.debug("in make_weights, he init parameters:{0}".format(parameters))
             else:
