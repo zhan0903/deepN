9bbe6ced1ab88dd1f24e1be75d17941df1654121
zhan0903 <z.han0903@qq.com>
2018-11-01 10:55:34 +1100

    multipule games version debug

--- gpu_implementation/neuroevolution/models/base.py
+++ gpu_implementation/neuroevolution/models/base.py
@@ -42,7 +42,7 @@ console_handler = logging.StreamHandler()
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
-logger.setLevel(level=logging.DEBUG)
+logger.setLevel(level=logging.INFO)
 
 
 class Net(nn.Module):
