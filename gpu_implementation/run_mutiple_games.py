f9614bd9fd058ae247973d9cbe07097255673791
zhan0903 <z.han0903@qq.com>
2018-11-01 12:15:21 +1100

    multipule games version test

--- gpu_implementation/run_mutiple_games.py
+++ gpu_implementation/run_mutiple_games.py
@@ -1,6 +1,6 @@
 import subprocess
 
-for i in range(2):
-    cmd = "python ga.py ./configurations/ga_atari_debug%s.json" % i
+for i in range(4):
+    cmd = "python ga.py ./configurations/ga_atari_test%s.json" % i
     returned_value = subprocess.call(cmd, shell=True)
     print('returned value:', returned_value)
