c9254bc48d66e331b9030dd4455a274f9500b959
zhan0903 <z.han0903@qq.com>
2018-11-01 11:42:30 +1100

    multipule games version debug

--- gpu_implementation/run_mutiple_games.py
+++ gpu_implementation/run_mutiple_games.py
@@ -1,6 +1,11 @@
 import subprocess
 
 
-cmd = "python ga.py ./configurations/ga_atari_config_debug.json"
-returned_value = subprocess.call(cmd, shell=True)
-print('returned value:', returned_value)
+cmd0 = "python ga.py ./configurations/ga_atari_config_debug.json"
+cmd1 = "python ga.py ./configurations/ga_atari_debug.json"
+
+for i in range(2):
+    cmd = cmd+"%d"%i
+    print(cmd)
+    returned_value = subprocess.call(cmd, shell=True)
+    print('returned value:', returned_value)
