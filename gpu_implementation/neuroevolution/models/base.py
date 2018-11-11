bd75d5eaa1af4d55317b2c1fd53c9de3e1de7d31
zhan0903 <z.han0903@qq.com>
2018-11-11 21:33:16 +1100

    test original garvitar for 6 hours

--- gpu_implementation/neuroevolution/models/base.py
+++ gpu_implementation/neuroevolution/models/base.py
@@ -40,7 +40,7 @@ console_handler = logging.StreamHandler()
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
-logger.setLevel(level=logging.INFO)
+logger.setLevel(level=logging.CRITICAL)
 
 
 class Net(nn.Module):
