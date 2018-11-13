80b6286c5a63e63cc909a8cb900376e7472b6f95
zhan0903 <z.han0903@qq.com>
2018-11-14 10:54:52 +1100

    update log system, experiment system

--- gpu_implementation/neuroevolution/helper.py
+++ gpu_implementation/neuroevolution/helper.py
@@ -46,7 +46,7 @@ class SharedNoiseTable(object):
         # logger.debug("in sharednoisetable, self.noise:{0},size of self.noise:{1}".format(self.noise, len(self.noise)))
         assert self.noise.dtype == np.float32
         logger.debug("code_type:{0},type:{1}".format(code_type, type(code_type)))
-        if str(code_type) is "mask":
+        if code_type == "mask":
             np.random.seed(seed)
             mask = np.random.choice(2, count, p=[0.6, 0.4]) # # 0->0.6,1->0.3
         else:
