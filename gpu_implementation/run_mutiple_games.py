9bcfccc2f7b477409dbe9c6bb6b7a48c00f33c21
zhan0903 <z.han0903@qq.com>
2018-11-27 15:02:20 +1100

    real mask

--- gpu_implementation/run_mutiple_games.py
+++ gpu_implementation/run_mutiple_games.py
@@ -1,6 +1,6 @@
 import subprocess
 
-for i in range(4,13):
+for i in range(6,13):
     cmd = "python ga.py ./configurations/ga_atari_test%s.json" % i
     returned_value = subprocess.call(cmd, shell=True)
     print('returned value:', returned_value)
