3288c11b83647715c5266975e171fe4a88887d5c
zhan0903 <z.han0903@gmail.com>
2018-11-27 15:22:19 +1100

    .gitignore fix

--- gpu_implementation/neuroevolution/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .dqn_xavier import SmallDQN, LargeDQN
-from .dqn import Model, LargeModel
-from .batchnorm import ModelBN, ModelVirtualBN
-from .simple import LinearClassifier, SimpleClassifier
--- gpu_implementation/run_mutiple_games.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-for i in range(6,13):
-    cmd = "python ga.py ./configurations/ga_atari_test%s.json" % i
-    returned_value = subprocess.call(cmd, shell=True)
-    print('returned value:', returned_value)
